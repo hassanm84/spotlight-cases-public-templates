0dabff92541402d5681c1f93ed30056606cc5493
ingenieur84 <m.h.masood@live.co.uk>
2021-03-22 17:58:43 +0000

    refactor: included a function for displaying error messages, update heading for case progress section.

--- case-details.php
+++ case-details.php
@@ -4,6 +4,25 @@ Template Name: Case Details
 */
 get_header();
 
+
+function displayExceptionMsg($message){
+    $excpMsg ='
+                <div id="main-content">
+                <div class="et-l et-l--body">
+                    <div class="et_builder_inner_content et_pb_gutters3">
+                        <div class="et_pb_section et_pb_section_0_tb_body et_section_regular">
+                            <div class="et_pb_row et_pb_row_0_tb_body et_pb_equal_columns">
+                                <div class="et_pb_column et_pb_column_2_5 et_pb_column_0_tb_body et_pb_css_mix_blend_mode_passthrough">
+                                <h1>'.$message.'</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>';
+
+    echo  $excpMsg;          
+}
+
 if(get_query_var('case_id')!==''){
     $caseId=get_query_var('case_id');
 
@@ -15,6 +34,24 @@ if(get_query_var('case_id')!==''){
     $docs_table = $wpdb->prefix .'cr_docs';
     $sections_table = $wpdb->prefix .'cr_sections';
     $updates_table = $wpdb->prefix .'cr_updates';
+
+    $cr_db_tbl_array = [$cases_table, $countries_table, $categories_table, $docs_table, $sections_table, $updates_table];
+    $tableNotExists = false;
+    foreach($cr_db_tbl_array as $cr_db_tbl){
+        if ( $wpdb->get_var( $wpdb->prepare( "SHOW TABLES LIKE %s", $cr_db_tbl ) ) === $cr_db_tbl ) {
+            //echo 'Table exists' . ': '. $cr_db_tbl;
+        } 
+        else {
+            //echo 'Table does not exist' . ': '. $cr_db_tbl;
+            $tableNotExists=TRUE;
+        }
+    }
+
+    if($tableNotExists){
+        displayExceptionMsg("This page is not available.");
+        get_footer();
+    }
+
 
     //Retrieve basic case information
     $query_cases="SELECT  
@@ -38,26 +75,36 @@ if(get_query_var('case_id')!==''){
     $results_cases = $wpdb->get_results ($query_cases, ARRAY_A);
     
     if ($wpdb->last_error) {
-        var_dump($wpdb->last_error);
+        //var_dump($wpdb->last_error);
+        get_footer();
+        exit();
     }
     
     //Retrieve all documents
     $results_docs = $wpdb->get_results ("SELECT * FROM  $docs_table WHERE case_id='$case_id'");
     
     if ($wpdb->last_error){
+        displayExceptionMsg("An error occured!");
+        get_footer();
+        exit();
     }
 
     //Retrieve all custom sections
     $results_sects = $wpdb->get_results ("SELECT * FROM  $sections_table WHERE case_id='$case_id'");
     if ($wpdb->last_error){
+        displayExceptionMsg("An error occured!");
+        get_footer();
+        exit();
     }
 
     //Retrieve all updates
     $results_updates = $wpdb->get_results ("SELECT * FROM  $updates_table WHERE case_id='$case_id'");
     if ($wpdb->last_error){
-
+        displayExceptionMsg("An error occured!");
+        get_footer();
+        exit();
     }
-        
+
 
 
 
@@ -93,14 +140,17 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
             <div class="et_pb_row et_pb_row_0_tb_body et_pb_equal_columns">
                <div class="et_pb_column et_pb_column_2_5 et_pb_column_0_tb_body  et_pb_css_mix_blend_mode_passthrough">
                   <div class="et_pb_module et_pb_image et_pb_image_0_tb_body et_pb_image_sticky">
-                     <span class="et_pb_image_wrap "><img loading="lazy" src="https://usercontent.one/wp/www.spotlightcorruption.org/wp-content/uploads/2021/02/photo-1534526490142-6c7ff3e05792.png" alt="" title="photo-1534526490142-6c7ff3e05792" height="auto" width="auto" srcset="https://usercontent.one/wp/www.spotlightcorruption.org/wp-content/uploads/2021/02/photo-1534526490142-6c7ff3e05792.png 1050w, https://usercontent.one/wp/www.spotlightcorruption.org/wp-content/uploads/2021/02/photo-1534526490142-6c7ff3e05792-980x653.png 980w, https://usercontent.one/wp/www.spotlightcorruption.org/wp-content/uploads/2021/02/photo-1534526490142-6c7ff3e05792-480x320.png 480w" sizes="(min-width: 0px) and (max-width: 480px) 480px, (min-width: 481px) and (max-width: 980px) 980px, (min-width: 981px) 1050px, 100vw" class="wp-image-2698"></span>
+           
+                     <span class="et_pb_image_wrap "><img loading="lazy" src="<?php echo $results_cases[0]['featured_img'];?>" class="wp-image-2698"></span>
                   </div>
                </div><!-- .et_pb_column -->
                <div class="et_pb_column et_pb_column_3_5 et_pb_column_1_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child">
                   <div class="et_pb_module et_pb_text et_pb_text_0_tb_body  et_pb_text_align_left et_pb_bg_layout_light">
                      <div class="et_pb_text_inner">
                         <h1><?php echo $results_cases[0]['title']; ?></h1>
-                     </div>
+                        
+                     
+                    </div>
                   </div>
                   <!-- .et_pb_text -->
                   <div class="et_pb_module et_pb_text et_pb_text_1_tb_body  et_pb_text_align_left et_pb_bg_layout_light">
@@ -172,13 +222,10 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
                                         </div>
                                     </div><!-- .et_pb_text -->
 
-
-
-
                                     <div class="et_pb_module et_pb_text et_pb_text_1  et_pb_text_align_left et_pb_bg_layout_light">
                                         <div class="et_pb_text_inner">
 
-                                        <p><h4><b>Updates</b></h4></p>
+                                        <p><h4><b>Progress of the cases in the UK</b></h4></p>
                                           <?php 
                                           $updateCount=0;
                                           foreach($results_updates as $update){
